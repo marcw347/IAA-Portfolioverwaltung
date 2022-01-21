9d012595f8138e6324414575708fc01c5748585c
Marc Wortmann <marc.wortmann@nordakademie.de>
2022-01-21 18:42:05 +0100

    Registrierung HTML

--- client/src/app/log-in-dialog/log-in-dialog.component.ts
+++ client/src/app/log-in-dialog/log-in-dialog.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
-    selector: 'log-in-dialog',
+    selector: 'app-log-in-dialog',
     templateUrl: './log-in-dialog.component.html',
     styleUrls: ['./log-in-dialog.component.css']
 })
