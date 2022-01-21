2ec57fa2c8d59d357c0513d76c9a0a89d1c06135
Marc Wortmann <marc.wortmann@nordakademie.de>
2022-01-21 14:13:20 +0100

    Registrierung HTML

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,38 +2,30 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {RoomsComponent} from './rooms/rooms.component';
+
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { RoomListComponent } from './rooms/room-list/room-list.component';
-import { RoomsFormComponent } from './rooms/rooms-form/rooms-form.component';
-import { MinValidatorDirective } from './rooms/shared/min-validator.directive';
+
+
 import { RegistrationDialogComponent} from "./registration-dialog/registration-dialog.component";
-import {CreateIssueComponent} from "./create-issue/create-issue.component";
+
 import {LogInDialogComponent} from "./log-in-dialog/log-in-dialog.component";
-import {BugTableComponent} from "./bug-table/bug-table.component";
-import {MenuComponent} from "./menu/menu.component"
-import {BugDetailledViewComponent} from "./bug-detailled-view/bug-detailled-view.component";
-import {AddCommentComponent} from "./add-comment/add-comment.component";
-import {CommentListComponent} from "./comment-list/comment-list.component";
+
+import {MenuComponent} from "./menu/menu.component";
+
 import {LogOffDialogComponent} from "./log-off-dialog/log-off-dialog.component";
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        RoomsComponent,
-        RoomListComponent,
-        RoomsFormComponent,
-        MinValidatorDirective,
+
         RegistrationDialogComponent,
-        CreateIssueComponent,
+
         LogInDialogComponent,
-        BugTableComponent,
+
         MenuComponent,
-        BugDetailledViewComponent,
-        AddCommentComponent,
-        CommentListComponent,
+
         LogOffDialogComponent,
 
     ],
